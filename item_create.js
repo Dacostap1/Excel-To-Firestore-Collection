46f6e2a48ffdf61cee01286ce5cb742fd94c72b7
Daniel Acosta <d4ni3L_15@hotmail.com>
2022-05-28 22:58:41 -0500

    fork project

--- item_create.js
+++ item_create.js
@@ -1,34 +1,32 @@
-const { Firestore } = require('@google-cloud/firestore');
-require('dotenv').config();
+const { Firestore } = require("@google-cloud/firestore");
+require("dotenv").config();
 
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
-console.log(CREDENTIALS)
+console.log(CREDENTIALS);
 
 const firestore = new Firestore({
-    projectId: CREDENTIALS.project_id,
-    credentials: {
-        client_email: CREDENTIALS.client_email,
-        private_key: CREDENTIALS.private_key
-    }
+  projectId: CREDENTIALS.project_id,
+  credentials: {
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  },
 });
 
-const sample_collection = firestore.collection('sample_collection');
+const sample_collection = firestore.collection("students");
 
 const createMenuItem = async (record) => {
-
-    try {
-        await sample_collection.add(record);
-        console.log('Records created.');
-    } catch (error) {
-        console.log(`Error at createRecord --> ${error}`);
-    }
+  try {
+    await sample_collection.add(record);
+    console.log("Records created.");
+  } catch (error) {
+    console.log(`Error at createRecord --> ${error}`);
+  }
 };
 
-let database = require('./sample_database.json');
+let database = require("./students.json");
 
 for (let index = 0; index < database.length; index++) {
-    let element = database[index];
-    element['isActive'] = true;
-    createMenuItem(element);
+  let element = database[index];
+  createMenuItem(element);
 }
